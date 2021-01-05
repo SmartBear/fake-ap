b9d013902cd294ab46e6a2e18e08d4abcd62789a
Mathias COQBLIN <mathias.coqblin@hiptest.net>
2021-01-06 00:50:48 +0100

    Add tests to Flags component

--- src/components/Flags/index.jsx
+++ src/components/Flags/index.jsx
@@ -19,62 +19,6 @@ import {
 } from './styled'
 import events from 'modules/events'
 
-const Flag = ({ title, body, type, actions, closeFlag }) => {
-  const onActionClick = actionIdentifier => {
-    events.emit('flag.action', { actionIdentifier })
-  }
-
-  let TypeIcon = InfoIcon
-  let iconColor = '#6554C0'
-
-  switch (type) {
-    case 'info':
-      TypeIcon = InfoIcon
-      iconColor = '#6554C0'
-      break
-    case 'success':
-      TypeIcon = SuccessIcon
-      iconColor = '#36B37E'
-      break
-    case 'warning':
-      TypeIcon = WarningIcon
-      iconColor = '#FF991F'
-      break
-    case 'error':
-      TypeIcon = ErrorIcon
-      iconColor = '#DE350B'
-  }
-
-  actions = Object.entries(actions).map(([actionIdentifier, text]) => ({ actionIdentifier, text }))
-
-  return (
-    <FlagContainer>
-      <TypeIconContainer color={iconColor}>
-        <TypeIcon />
-      </TypeIconContainer>
-      <CloseIconContainer onClick={closeFlag}>
-        <CloseIcon />
-      </CloseIconContainer>
-      <Message>
-        <Title>{title}</Title>
-        {body}
-        <Actions>
-          {actions.map(
-            action => (
-              <ActionButton
-                key={action.actionIdentifier}
-                onClick={() => onActionClick(action.actionIdentifier)}
-              >
-                <ActionText>{action.text}</ActionText>
-              </ActionButton>
-            )
-          )}
-        </Actions>
-      </Message>
-    </FlagContainer>
-  )
-}
-
 const Flags = () => {
   const [flags, setFlags] = useState([])
   const [timeouts, setTimeouts] = useState({})
@@ -130,6 +74,71 @@ const Flags = () => {
       {flags.map(flag => <Flag key={flag.id} {...flag.options} closeFlag={() => closeFlag(flag.id)} />)}
     </FlagsContainer>
   )
+}
+
+const Flag = ({ title, body, type, actions, closeFlag }) => {
+  const onActionClick = actionIdentifier => {
+    events.emit('flag.action', { actionIdentifier })
+  }
+
+  let TypeIcon = InfoIcon
+  let iconColor = '#6554C0'
+
+  switch (type) {
+    case 'info':
+      TypeIcon = InfoIcon
+      iconColor = '#6554C0'
+      break
+    case 'success':
+      TypeIcon = SuccessIcon
+      iconColor = '#36B37E'
+      break
+    case 'warning':
+      TypeIcon = WarningIcon
+      iconColor = '#FF991F'
+      break
+    case 'error':
+      TypeIcon = ErrorIcon
+      iconColor = '#DE350B'
+  }
+
+  actions = Object.entries(actions).map(([actionIdentifier, text]) => ({ actionIdentifier, text }))
+
+  return (
+    <FlagContainer data-testid='ap-flag'>
+      <TypeIconContainer color={iconColor} data-testid='ap-flag-type'>
+        <TypeIcon />
+      </TypeIconContainer>
+      <CloseIconContainer onClick={closeFlag} data-testid='ap-flag-close'>
+        <CloseIcon />
+      </CloseIconContainer>
+      <Message>
+        <Title data-testid='ap-flag-title'>{title}</Title>
+        {body}
+        <Actions>
+          {actions.map(
+            action => (
+              <ActionButton
+                key={action.actionIdentifier}
+                onClick={() => onActionClick(action.actionIdentifier)}
+                data-testid='ap-flag-action'
+              >
+                <ActionText>{action.text}</ActionText>
+              </ActionButton>
+            )
+          )}
+        </Actions>
+      </Message>
+    </FlagContainer>
+  )
+}
+
+Flag.defaultProps = {
+  title: '',
+  body: '',
+  type: 'info',
+  actions: {},
+  closeFlag: () => {}
 }
 
 export default Flags
--- test/components/Flags/index.spec.js
+++ test/components/Flags/index.spec.js
@@ -1,14 +1,229 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent, act, within } from '@testing-library/react'
+import events from 'modules/events'
+import { InfoIcon, SuccessIcon, WarningIcon, ErrorIcon } from 'components/Flags/icons'
 import Flags from 'components/Flags'
 
-describe('Flags', () => {
-  it('renders an empty container at the top right corner', () => {
-    const flags = render(<Flags />)
+jest.useFakeTimers()
 
+describe('Flags', () => {
+  let flags = null
+
+  beforeEach(() => {
+    flags = render(<Flags />)
+  })
+
+  it('renders an empty container at the top right corner', () => {
     const container = flags.queryByTestId('ap-flags')
 
     expect(container).toBeEmptyDOMElement()
     expect(container).toHaveStyle({ position: 'fixed', top: '0', right: '30px' })
+  })
+
+  describe('when a flag is created', () => {
+    it('displays the flag with the title and body', () => {
+      const options = { title: 'Title', body: 'Body' }
+
+      act(() => {
+        events.emit('flag.create', { id: 1, options })
+      })
+
+      expect(flags.queryByText('Title')).toBeInTheDocument()
+      expect(flags.queryByText('Body')).toBeInTheDocument()
+    })
+
+    it('adds the flag to existing flags', () => {
+      const firstFlagOptions = { title: 'First title', body: 'First body' }
+      const secondFlagOptions = { title: 'Second title', body: 'Second body' }
+
+      act(() => {
+        events.emit('flag.create', { id: 1, options: firstFlagOptions })
+        events.emit('flag.create', { id: 2, options: secondFlagOptions })
+      })
+
+      expect(flags.queryByText('First title')).toBeInTheDocument()
+      expect(flags.queryByText('First body')).toBeInTheDocument()
+      expect(flags.queryByText('Second title')).toBeInTheDocument()
+      expect(flags.queryByText('Second body')).toBeInTheDocument()
+    })
+  })
+
+  describe('flag type', () => {
+    it('can display an info flag', () => {
+      const options = { type: 'info' }
+
+      act(() => {
+        events.emit('flag.create', { id: 1, options })
+      })
+
+      const infoIcon = render(<InfoIcon />).container
+
+      expect(flags.getByTestId('ap-flag-type').innerHTML).toEqual(infoIcon.innerHTML)
+    })
+
+    it('can display a success flag', () => {
+      const options = { type: 'success' }
+
+      act(() => {
+        events.emit('flag.create', { id: 1, options })
+      })
+
+      const successIcon = render(<SuccessIcon />).container
+
+      expect(flags.getByTestId('ap-flag-type').innerHTML).toEqual(successIcon.innerHTML)
+    })
+
+    it('can display a warning flag', () => {
+      const options = { type: 'warning' }
+
+      act(() => {
+        events.emit('flag.create', { id: 1, options })
+      })
+
+      const warningIcon = render(<WarningIcon />).container
+
+      expect(flags.getByTestId('ap-flag-type').innerHTML).toEqual(warningIcon.innerHTML)
+    })
+
+    it('can display an error flag', () => {
+      const options = { type: 'error' }
+
+      act(() => {
+        events.emit('flag.create', { id: 1, options })
+      })
+
+      const errorIcon = render(<ErrorIcon />).container
+
+      expect(flags.getByTestId('ap-flag-type').innerHTML).toEqual(errorIcon.innerHTML)
+    })
+
+    it('defaults to an info flag', () => {
+      const options = {}
+
+      act(() => {
+        events.emit('flag.create', { id: 1, options })
+      })
+
+      const infoIcon = render(<InfoIcon />).container
+
+      expect(flags.getByTestId('ap-flag-type').innerHTML).toEqual(infoIcon.innerHTML)
+    })
+  })
+
+  describe('actions', () => {
+    beforeEach(() => {
+      const options = { actions: { identifier: 'action name' } }
+
+      act(() => {
+        events.emit('flag.create', { id: 1, options })
+      })
+    })
+
+    it('renders a button with the action name', () => {
+      expect(flags.getByTestId('ap-flag-action')).toHaveTextContent('action name')
+    })
+
+    it('triggers a flag.action event including the action identifier', () => {
+      const listener = jest.fn()
+      events.on('flag.action', listener)
+
+      fireEvent.click(flags.getByTestId('ap-flag-action'))
+
+      expect(listener).toHaveBeenCalledWith({ actionIdentifier: 'identifier' })
+    })
+  })
+
+  describe('close button', () => {
+    beforeEach(() => {
+      const firstFlagOptions = { title: 'First title' }
+      const secondFlagOptions = { title: 'Second title' }
+      const thirdFlagOptions = { title: 'Third title' }
+
+      act(() => {
+        events.emit('flag.create', { id: 1, options: firstFlagOptions })
+        events.emit('flag.create', { id: 2, options: secondFlagOptions })
+        events.emit('flag.create', { id: 3, options: thirdFlagOptions })
+      })
+    })
+
+    it('removes the flag from the list of flags', () => {
+      const secondFlag = flags.getAllByTestId('ap-flag').find(
+        flag => within(flag).getByTestId('ap-flag-title').textContent === 'Second title'
+      )
+
+      fireEvent.click(within(secondFlag).getByTestId('ap-flag-close'))
+
+      expect(flags.queryByText('Second title')).not.toBeInTheDocument()
+    })
+  })
+
+  describe('close event', () => {
+    beforeEach(() => {
+      const firstFlagOptions = { title: 'First title' }
+      const secondFlagOptions = { title: 'Second title' }
+      const thirdFlagOptions = { title: 'Third title' }
+
+      act(() => {
+        events.emit('flag.create', { id: 1, options: firstFlagOptions })
+        events.emit('flag.create', { id: 2, options: secondFlagOptions })
+        events.emit('flag.create', { id: 3, options: thirdFlagOptions })
+      })
+    })
+
+    it('is possible to close a flag using events and the flag ID', () => {
+      act(() => {
+        events.emit('flag.close', 2)
+      })
+
+      expect(flags.queryByText('Second title')).not.toBeInTheDocument()
+    })
+  })
+
+  describe('close option', () => {
+    beforeEach(() => {
+      const firstFlagOptions = { title: 'Title 1', close: 'manual' }
+      const secondFlagOptions = { title: 'Title 2', close: 'auto' }
+      const thirdFlagOptions = { title: 'Title 3' }
+
+      act(() => {
+        events.emit('flag.create', { id: 1, options: firstFlagOptions })
+        events.emit('flag.create', { id: 2, options: secondFlagOptions })
+        events.emit('flag.create', { id: 3, options: thirdFlagOptions })
+      })
+    })
+
+    it('does not close flags with close option set to manual', () => {
+      expect(flags.queryByText('Title 1')).toBeInTheDocument()
+
+      act(() => {
+        jest.runAllTimers()
+      })
+
+      expect(flags.queryByText('Title 1')).toBeInTheDocument()
+    })
+
+    it('closes flags with close option set to auto after 4 seconds', () => {
+      expect(flags.queryByText('Title 2')).toBeInTheDocument()
+
+      jest.advanceTimersByTime(2000)
+
+      expect(flags.queryByText('Title 2')).toBeInTheDocument()
+
+      act(() => {
+        jest.advanceTimersByTime(2000)
+      })
+
+      expect(flags.queryByText('Title 2')).not.toBeInTheDocument()
+    })
+
+    it('does not close flags with no close option', () => {
+      expect(flags.queryByText('Title 3')).toBeInTheDocument()
+
+      act(() => {
+        jest.runAllTimers()
+      })
+
+      expect(flags.queryByText('Title 3')).toBeInTheDocument()
+    })
   })
 })
