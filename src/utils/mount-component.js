b1f2b97b796260fae7689822d5d30664e6bc816c
Mathias COQBLIN <43134913+mcoqblin-hiptest@users.noreply.github.com>
2022-03-30 13:24:03 +0200

    Make the package compatible with the previous versions of React

--- src/utils/mount-component.js
+++ src/utils/mount-component.js
@@ -1,14 +1,9 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 
 const roots = {}
 
 const mountComponentWhenDocumentIsReady = (component, id) => {
   onDocumentReady(() => mountComponent(component, id))
-}
-
-const unmountComponent = id => {
-  roots[id]?.unmount()
-  roots[id] = null
 }
 
 const onDocumentReady = callback => {
@@ -33,10 +28,26 @@ const mountComponent = (component, id) => {
     document.body.appendChild(container)
   }
 
-  const root = ReactDOM.createRoot(container)
+  const root = createRoot(container)
   roots[id] = root
 
   root.render(component)
+}
+
+const createRoot = () => {
+  if (typeof ReactDOM.createRoot === 'function') {
+    return ReactDOM.createRoot
+  }
+
+  return container => ({
+    render: component => ReactDOM.render(component, container),
+    unmount: () => ReactDOM.unmountComponentAtNode(container)
+  })
+}
+
+const unmountComponent = id => {
+  roots[id]?.unmount()
+  roots[id] = null
 }
 
 export {
